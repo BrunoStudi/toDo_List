b0bd6afd69a3287d95d566f94d6cb58fa8e9dea5
BrunoStudi <r.brunocarriere@protonmail.com>
2024-05-26 17:44:31 +0200

    ajout de la possibilité d'editer son profil depuis ma page profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserFormType;
 use App\Repository\TodoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +21,29 @@ class ProfilController extends AbstractController
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
             'todo' => $todo
+        ]);
+    }
+
+    #[Route('profil/edit/{id}', name: 'edt_profil', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        // Création du formulaire de modification de la todo
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        // Vérification de la soumission du formulaire et de sa validité
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Enregistrement des modifications en base de données
+            $entityManager->flush();
+
+            // Redirection vers la liste des todos
+            return $this->redirectToRoute('app_profil', [], Response::HTTP_SEE_OTHER);
+        }
+
+        // Affichage du formulaire de modification de la todo.
+        return $this->render('profil/edit.html.twig', [
+            'user' => $user,
+            'userForm' => $form,
         ]);
     }
 }
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('avatar', FileType::class, [
+                'label' => 'Votre avatar:',
+                // unmapped signifie que le champ n'est pas associé à une propriété d'entité.
+                'mapped' => false,
+                /* rendez-le facultatif afin que vous n'ayez pas à télécharger à nouveau le fichier
+                a chaque fois que vous editez les details de la recette.*/
+                'required' => false,
+                /* les champs unmapped ne peuvent pas définir leur validation à l'aide d'attributs
+                dans l'entité associée, vous pouvez donc utiliser les classes de contraintes PHP*/
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Téléversez une image valide',
+                    ])
+                ],
+            ])
+            ->add('pseudo')
+            ->add('surname')
+            ->add('name')
+            ->add('email', EmailType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
