3a681e891d588169740bd4c347d2f434c0d7cf88
BrunoStudi <r.brunocarriere@protonmail.com>
2024-08-12 14:19:29 +0200

    ajout du nombre de tâches en cours, terminées et totales sur la page d'accueil du site lorsque l'utilisateur est connecté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TodoRepository;
 use App\Repository\SettingsRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,15 +13,23 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(
+        TodoRepository $todoRepo,
         SettingsRepository $settingsRepo, 
         Request $request): Response
     {
         $session = $request->getSession();
         $data = $settingsRepo->findAll();
         $session->set("setting", $data[0]);
+
+        $nblst = $todoRepo->findAll();
+        $nblstDone = $todoRepo->findBy(['etat' => 1 ]);
+        $nblstInProgress = $todoRepo->findBy(['etat' => 0]);
         
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+            'nblst' => $nblst,
+            'nblstDone' => $nblstDone,
+            'nblstInProgress' => $nblstInProgress
         ]);
     }
 
